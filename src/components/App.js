53a7625ce875f642eca0251071f09076a8a7e778
Bigone0617 <xodlfsha@gmail.com>
2020-09-18 00:22:02 +0900

    add log in

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 import AppRouter from './Router';
+import { authService } from "fbase";
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
   return (
-    <AppRouter/>
-  );
+    <>
+      <AppRouter isLoggedIn={isLoggedIn}/>
+  <footer>&copy;  {new Date().getFullYear()} Nwitter</footer>
+    </>
+    );
 }
 
 export default App;
